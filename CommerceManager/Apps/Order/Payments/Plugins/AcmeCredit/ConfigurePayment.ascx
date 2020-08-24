da7d5973e5090d91c5df3d8b104da7dfccd13227
Artur Vasyliev <princeartik@gmail.com>
2020-08-24 19:21:55 +0300

    Added missing files to CommerceManager project.

---
+++ CommerceManager/Apps/Order/Payments/Plugins/AcmeCredit/ConfigurePayment.ascx
@@ -0,0 +1,9 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ConfigurePayment.ascx.cs" Inherits="AcmePaymentProvider.ConfigurePayment" %>
+<table id="GenericTable" runat="server">
+    <tr>
+        <td class="FormLabelCell" colspan="2"><b><asp:Literal ID="litLabel" Text="Enter custom parameter value" runat="server"></asp:Literal></b></td>
+    </tr>
+    <tr>
+		<td class="FormLabelCell" colspan="2"><b><asp:TextBox ID="txtSecretKey" Text="Enter some key example." runat="server"></asp:TextBox></b></td>
+	</tr>
+</table>
---
+++ CommerceManager/IndexMoreProperties.cs
@@ -0,0 +1,38 @@
+﻿using EPiServer.ServiceLocation;
+using Mediachase.Commerce;
+using Mediachase.Commerce.Catalog;
+using Mediachase.Commerce.Catalog.Dto;
+using Mediachase.Commerce.InventoryService;
+using Mediachase.Commerce.Pricing;
+using Mediachase.MetaDataPlus;
+using Mediachase.Search.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommerceManager
+{
+    public class IndexMoreProperties : BaseCatalogIndexBuilder
+    {
+        private ICatalogSystem _catalog;
+        protected IndexMoreProperties(ICatalogSystem catalogSystem, IPriceService priceService, IInventoryService inventoryService, MetaDataContext metaDataContext, CatalogItemChangeManager catalogItemChangeManager, NodeIdentityResolver nodeIdentityResolver) : base(catalogSystem, priceService, inventoryService, metaDataContext, catalogItemChangeManager, nodeIdentityResolver)
+        {
+            _catalog = catalogSystem;
+        }
+
+        public IndexMoreProperties() : this(ServiceLocator.Current.GetInstance<ICatalogSystem>(),
+            ServiceLocator.Current.GetInstance<IPriceService>(),
+            ServiceLocator.Current.GetInstance<IInventoryService>(),
+            ServiceLocator.Current.GetInstance<MetaDataContext>(),
+            ServiceLocator.Current.GetInstance<CatalogItemChangeManager>(),
+            ServiceLocator.Current.GetInstance<NodeIdentityResolver>()
+            )
+        { }
+
+        protected override void OnCatalogEntryIndex(ref SearchDocument document, CatalogEntryDto.CatalogEntryRow entry, string language)
+        {
+            base.OnCatalogEntryIndex(ref document, entry, language);
+        }
+    }
+}
